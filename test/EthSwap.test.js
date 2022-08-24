d414037b2c94afb05280ac0e7f33bda2e9019317
Developer Junaid <qjunaid623@gmail.com>
2022-08-25 00:15:54 +0500

    added first truffle chai test

---
+++ test/EthSwap.test.js
@@ -0,0 +1,20 @@
+const Token = artifacts.require("Token")
+const EthSwap = artifacts.require("EthSwap")
+
+// Configure Chai
+require("chai")
+  .use(require("chai-as-promised"))
+  .should()
+
+contract("EthSwap", (accounts) => {
+  // Tests for EthSwap contract
+
+  // Test that smart contract was deployed
+  describe("EthSwap deployment", async () => {
+    it("contract has a name", async () => {
+      const ethSwap = await EthSwap.new()
+      const name = await ethSwap.name()
+      assert.equal(name, "EthSwap Instant Exchange")
+    })
+  })
+})
