eae5250164693b1799e1cfeb7dd4be05c6769663
Developer Junaid <qjunaid623@gmail.com>
2022-08-25 21:07:11 +0500

    created tests

---
+++ readme.md
@@ -0,0 +1,4 @@
+## Unit (1 Ether)
+
+- 1 USD = 100 Cents
+- 1 Ether = 1000000000000000000 Wei (18 decimals)
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -6,23 +6,39 @@ require("chai")
   .use(require("chai-as-promised"))
   .should()
 
+// Function for converting tokens to wei
+function tokens(n) {
+  return web3.utils.toWei(n, "ether")
+}
+
 contract("EthSwap", (accounts) => {
   // Tests for EthSwap contract
+  let token, ethSwap
+
+  // Before hook for variables
+  before(async () => {
+    token = await Token.new()
+    ethSwap = await EthSwap.new()
+
+    await token.transfer(ethSwap.address, tokens("1000000")) // Transfer tokens from token contract to ethswap contract
+  })
 
   describe("Token deployment", async () => {
     it("contract has a name", async () => {
-      const token = await Token.new()
       const name = await token.name()
       assert.equal(name, "Junaid Token")
     })
   })
 
-  // Test that smart contract was deployed
   describe("EthSwap deployment", async () => {
     it("contract has a name", async () => {
-      const ethSwap = await EthSwap.new()
       const name = await ethSwap.name()
       assert.equal(name, "EthSwap Instant Exchange")
     })
+  })
+
+  it("contract has tokens", async () => {
+    let balance = await token.balanceOf(ethSwap.address) // Get balance of ethswap contract
+    assert.equal(balance.toString(), tokens("1000000")) // Check if it is equal to tokens sent
   })
 })
